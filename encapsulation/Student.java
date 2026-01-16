37bc1d486dcf9a6844714a6c4bf9381239c3933b
wishyourcode <vishalm3101@gmail.com>
2026-01-16 23:56:01 +0530

    encapsulation 3rd example

---
+++ encapsulation/Student.java
@@ -0,0 +1,57 @@
+package encapsulation;
+
+public class Student {
+    private int id;
+    private String name;
+    private String course;
+    private int marks;
+    private int age;
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getcourse() {
+        return course;
+    }
+
+    public void setcourse(String course) {
+        this.course = course;
+    }
+
+    public int getmarks() {
+        return marks;
+    }
+
+    public void setmarks(int marks) {
+        this.marks = marks;
+    }
+
+    public int getage() {
+        return age;
+    }
+
+    public void setage(int age) {
+        this.age = age;
+    }
+}
+
+class Driver {
+    public static void main(String[] args) {
+        Student S1 = new Student();
+        S1.setcourse("IT");
+        System.out.println(S1.getcourse());
+    }
+}
