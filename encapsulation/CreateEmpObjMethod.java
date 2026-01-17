24abf42fbf926110db48d438941de798ad9c0471
wishyourcode <vishalm3101@gmail.com>
2026-01-17 23:55:29 +0530

    Employee

--- encapsulation/CreateEmpObjMethod.java
+++ encapsulation/CreateEmpObjMethod.java
@@ -1,32 +1,18 @@
 package encapsulation;
 
 class Employee {
-    private String empName;
-    private int empId;
-    private double empSalary;
+    String empName;
+    int empId;
+    double empSalary;
 
     public static void createEmpObj(Employee emp) {
         System.out.println("Employee Object Created" + emp);
     }
+}
 
-    // Method to create and initialize employee object
-    public void createEmployee(String name, int id, double salary) {
-        this.empName = name;
-        this.empId = id;
-        this.empSalary = salary;
-    }
-
-    // Method to display employee details
-    public void displayEmployee() {
-        System.out.println("Employee Name: " + empName);
-        System.out.println("Employee ID: " + empId);
-        System.out.println("Employee Salary: " + empSalary);
-    }
-
+public class CreateEmpObjMethod {
     public static void main(String[] args) {
         Employee E1 = new Employee();
-        createEmpObj(E1);
-        E1.createEmployee("Vishal Mishra", 101, 55000);
-        E1.displayEmployee();
+        Employee.createEmpObj(E1);
     }
 }
