aed023029e4644e22338ad3b9848bfb37c99da6f
wishyourcode <vishalm3101@gmail.com>
2026-01-17 10:12:41 +0530

    4th encapsulation Example

---
+++ encapsulation/Employee.java
@@ -0,0 +1,59 @@
+package encapsulation;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private int id;
+    private String department;
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public double getsalary() {
+        return salary;
+    }
+
+    public void setsalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getid() {
+        return id;
+    }
+
+    public void setid(int id) {
+        this.id = id;
+    }
+
+    public String getdepartment() {
+        return department;
+    }
+
+    public void setdepartment(String department) {
+        this.department = department;
+    }
+
+    public void display() {
+        System.out.println(getid());
+        System.out.println(getname());
+        System.out.println(getsalary());
+        System.out.println(getdepartment());
+    }
+}
+
+class InnerEmployee {
+
+    public static void main(String[] args) {
+        Employee E1 = new Employee();
+        E1.setid(1);
+        E1.setname("Vishal");
+        E1.setsalary(69000);
+        E1.setdepartment("IT");
+        E1.display();
+    }
+}
