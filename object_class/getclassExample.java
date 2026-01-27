0f163a12cb6a8807e69bb0a85d8bd7919cd120ae
wishyourcode <vishalm3101@gmail.com>
2026-01-27 22:05:06 +0530

    getClass() return the class name of the class

---
+++ object_class/getclassExample.java
@@ -0,0 +1,10 @@
+package object_class;
+
+public class getclassExample {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+
+        System.out.println(d.getClass());
+        System.out.println(d.getClass().getName());
+    }
+}
