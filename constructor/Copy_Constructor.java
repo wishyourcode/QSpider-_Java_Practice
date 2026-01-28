66ef6098f3b9a74b8687d246082fbec4b1d4653b
wishyourcode <vishalm3101@gmail.com>
2026-01-28 19:55:45 +0530

    Copy Constructor

---
+++ constructor/Copy_Constructor.java
@@ -0,0 +1,37 @@
+package constructor;
+
+class Example {
+    private int a;
+    private int b;
+
+    Example(int a, int b) {
+        System.out.println("Main constructor called");
+        this.a = a;
+        this.b = b;
+    }
+
+    Example(Example c) {
+        this.a = c.a;
+        this.b = c.b;
+    }
+
+    public void Display() {
+        System.out.println("value of the a is : " + a);
+        System.out.println("value of the b is : " + b);
+    }
+}
+
+public class Copy_Constructor {
+
+    public static void main(String[] args) {
+        Example E1 = new Example(3, 5);
+        Example E2 = new Example(E1);
+        Example E3 = E2;
+        System.out.println("===============================");
+        E2.Display();
+        System.out.println("===============================");
+        E3.Display();
+        System.out.println("===============================");
+        E1.Display();
+    }
+}
