6fef362e4dd0a28e238ac3ff926ac4ef63688ee6
wishyourcode <vishalm3101@gmail.com>
2026-01-30 00:25:20 +0530

    Iphone model implemented

--- mobile/Iphone.java
+++ mobile/Iphone.java
@@ -2,6 +2,6 @@ package mobile;
 
 public class Iphone {
     public void PrintThank() {
-        System.out.println("Thank you for chooising :");
+        System.out.print("Thank you for chooising :");
     }
 }
