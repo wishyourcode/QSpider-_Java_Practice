1e4e13df08a49740803249277b6c2c62e3de1b73
wishyourcode <vishalm3101@gmail.com>
2026-02-05 22:02:43 +0530

    allowed and not allowed in abstraction

--- abstraction/Employee.java
+++ abstraction/Employee.java
@@ -4,15 +4,18 @@ abstract class Employee {
     int a = 9; // variable by default is non-static and can be accessed by instance methods
 
     public void printThanks() {
-        System.out.println("Thank you");
+        System.out.println("Thank you belong to Employee parents class");
     }
 
     abstract void printEmployeeType();
 
+    abstract int getEmployeeID(); // abstract method without body, to be implemented by child class with return
+                                  // type int
+
     public abstract void calculateSalary();
 
     public static void companyPolicy() {
-        System.out.println("Company Policy: Maintain professionalism at all times.");
+        System.out.println("Company Policy: belong to parent class ");
     }
 
     Employee() {
--- abstraction/Implementation.java
+++ abstraction/Implementation.java
@@ -3,18 +3,23 @@ package abstraction;
 public class Implementation extends Employee {
 
     @Override
-    void printEmployeeType() {
-        System.out.println("This is a full-time employee.");
+    public void printEmployeeType() {
+        System.out.println("this is child class method whose body is provided in child class");
     }
 
     @Override
     public void calculateSalary() {
-        System.out.println("Calculating salary for full-time employee.");
+        System.out.println("Calculating salary for employee in child class");
     }
 
     @Override
     public void printThanks() {
-        System.out.println("Thanks for being a valuable employee!");
+        System.out.println("Thanks for being to child class");
+    }
+
+    @Override
+    public int getEmployeeID() {
+        return 12345; // Example employee ID
     }
 
 }
