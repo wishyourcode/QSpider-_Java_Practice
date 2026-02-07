9c99432868176862f7de17e21a0ec0aa288d6137
wishyourcode <vishalm3101@gmail.com>
2026-02-07 20:07:32 +0530

    equals method overring

--- object_class/overriding.java
+++ object_class/overriding.java
@@ -5,15 +5,35 @@ public class overriding {
     String name;
     int age;
 
+    overriding(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
     public String toString() {
         return "Student id is " + id + " \nname is " + name + "\nage is " + age;
     }
 
+    public boolean equals(Object obj) {
+        overriding student = (overriding) obj;
+        if (this.id == student.id && this.name.equals(student.name) && this.age == student.age) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int hashCode() {
+        return id + name.hashCode() + age;
+    }
+
     public static void main(String[] args) {
-        overriding student = new overriding();
-        student.id = 1;
-        student.name = "Vishal";
-        student.age = 23;
+        overriding student = new overriding(1, "Vishal", 23);
+        overriding student1 = new overriding(1, "Vishal", 23);
+        overriding student2 = new overriding(2, "Vishal", 23);
+        System.out.println(student.equals(student1)); // Output: true
+        System.out.println(student.equals(student2)); // Output: false
         System.out.println(student); // Output: Student id is 1 and name is Vishal
         // and age is 23
     }
