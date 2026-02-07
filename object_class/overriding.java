62d25ddb56f9826bac21df7e6028d808a3289da8
wishyourcode <vishalm3101@gmail.com>
2026-02-07 20:14:12 +0530

    Hashcode override

--- object_class/overriding.java
+++ object_class/overriding.java
@@ -1,5 +1,7 @@
 package object_class;
 
+import java.util.Objects;
+
 public class overriding {
     int id;
     String name;
@@ -25,15 +27,19 @@ public class overriding {
     }
 
     public int hashCode() {
-        return id + name.hashCode() + age;
+        // return Integer.hashCode(id) + name.hashCode() + Integer.hashCode(age);
+        return Objects.hash(id, name, age);
     }
 
     public static void main(String[] args) {
         overriding student = new overriding(1, "Vishal", 23);
         overriding student1 = new overriding(1, "Vishal", 23);
         overriding student2 = new overriding(2, "Vishal", 23);
-        System.out.println(student.equals(student1)); // Output: true
-        System.out.println(student.equals(student2)); // Output: false
+        System.out.println("student hashcode is " + student.hashCode()); // Output: 123456789
+        System.out.println("student1 hashcode is " + student1.hashCode()); // Output
+        System.out.println(student.hashCode() == student1.hashCode()); // Output: 123456789
+        System.out.println("Is student is equal to student 1 : " + student.equals(student1)); // Output: true
+        System.out.println("Is student is equal to student 2 : " + student.equals(student2)); // Output: false
         System.out.println(student); // Output: Student id is 1 and name is Vishal
         // and age is 23
     }
