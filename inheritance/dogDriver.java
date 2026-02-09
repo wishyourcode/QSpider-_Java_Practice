de45cb955289979690d52df598d3e1fe7ae613a7
wishyourcode <vishalm3101@gmail.com>
2026-02-09 23:08:12 +0530

    Dog Driver implementation

---
+++ inheritance/dogDriver.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class dogDriver {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+        dog.eat();
+        dog.sleep();
+    }
+}
