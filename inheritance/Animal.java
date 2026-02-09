9a344125e47a0420da44e2bf7ab87fea34149d44
wishyourcode <vishalm3101@gmail.com>
2026-02-09 23:03:45 +0530

    Interface example

---
+++ inheritance/Animal.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Animal {
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+
+    public void eat() {
+        System.out.println("The animal is eating");
+    }
+
+    public void sleep() {
+        System.out.println("The animal is sleeping");
+    }
+}
