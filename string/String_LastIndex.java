c9fe13f3c3055a7338191a56e1ce216b2cc68a53
wishyourcode <vishalm3101@gmail.com>
2026-02-10 23:43:52 +0530

    String last index of method

---
+++ string/String_LastIndex.java
@@ -0,0 +1,9 @@
+package string;
+
+public class String_LastIndex {
+    public static void main(String[] args) {
+        String str = "Hello, welcome to the world of Java programming. Java is fun.";
+        int lastIndex = str.lastIndexOf("Java");
+        System.out.println("Last index of 'Java': " + lastIndex);
+    }
+}
