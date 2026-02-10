43377c18632ddac249ee6a8ffdffea42992a8ed3
wishyourcode <vishalm3101@gmail.com>
2026-02-10 23:42:22 +0530

    String Index of method

---
+++ string/String_IndexOf.java
@@ -0,0 +1,9 @@
+package string;
+
+public class String_IndexOf {
+    public static void main(String[] args) {
+        String str = "Hello, welcome to the world of Java programming.";
+        int index = str.indexOf("Java");
+        System.out.println("Index of 'Java': " + index);
+    }
+}
