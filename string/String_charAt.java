8b70b773fd43b7e603e70b2dc3a73e9cb90ef010
wishyourcode <vishalm3101@gmail.com>
2026-02-10 23:44:41 +0530

    String chatAt method

---
+++ string/String_charAt.java
@@ -0,0 +1,9 @@
+package string;
+
+public class String_charAt {
+    public static void main(String[] args) {
+        String str = "Hello, welcome to the world of Java programming.";
+        char ch = str.charAt(7);
+        System.out.println("Character at index 7: " + ch);
+    }
+}
