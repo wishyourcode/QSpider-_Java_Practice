64a7161a9bc4d51f56f5ef67b42c3d93257b4a97
wishyourcode <vishalm3101@gmail.com>
2026-02-10 23:48:28 +0530

    String Contains method

---
+++ string/String_contains.java
@@ -0,0 +1,9 @@
+package string;
+
+public class String_contains {
+    public static void main(String[] args) {
+        String str = "Hello, welcome to the world of Java programming.";
+        boolean containsJava = str.contains("Java");
+        System.out.println("Does the string contain 'Java'? " + containsJava);
+    }
+}
