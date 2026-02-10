32aa4290c75f0d8a3aa695320ac8f1be605752c9
wishyourcode <vishalm3101@gmail.com>
2026-02-10 23:39:05 +0530

    Length() method

---
+++ string/String_Length.java
@@ -0,0 +1,11 @@
+package string;
+
+public class String_Length {
+    public static void main(String[] args) {
+        String Name = "Java Programming";
+        int length = Name.length();
+        System.out.println("Length of the string: " + length);
+
+    }
+
+}
