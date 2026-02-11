e1d36cd3554bccd7e67aca57f3b2010285fee9e9
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:48:19 +0530

    String compareTo method

---
+++ string/String_CompareTo.java
@@ -0,0 +1,11 @@
+package string;
+
+public class String_CompareTo {
+    public static void main(String[] args) {
+        String name = "vishal";
+        String name2 = "vishal";
+        String name3 = "Vishal";
+        System.out.println(name.compareTo(name3));
+        System.out.println(name2.compareTo(name2));
+    }
+}
