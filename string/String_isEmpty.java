f2e5cfc2aaf813b16fe159334e4a75212373df8f
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:51:46 +0530

    String IsEmply and isBlank method

---
+++ string/String_isEmpty.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String_isEmpty {
+    public static void main(String[] args) {
+        String name = "";
+        String name1 = " ";
+        System.out.println(name.isEmpty());
+        System.out.println(name.isBlank());
+        System.out.println(name1.isBlank());
+        System.out.println(name1.isEmpty());
+
+    }
+}
