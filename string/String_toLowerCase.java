8c0228fd436fa6bbd2bc264971d8d6083cb5719f
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:26:26 +0530

    String to Lower case method

---
+++ string/String_toLowerCase.java
@@ -0,0 +1,10 @@
+package string;
+
+public class String_toLowerCase {
+    public static void main(String[] args) {
+        String name = "Radhe Radhe";
+        String loName = name.toLowerCase();
+        System.out.println(name);
+        System.out.println(loName);
+    }
+}
