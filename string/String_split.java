6f4e2e868893a38d6ca6e39e69dc27e0411bae14
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:44:20 +0530

    String split method

---
+++ string/String_split.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Arrays;
+
+public class String_split {
+    public static void main(String[] args) {
+        String name = "Radhe Radhe Radhe Radhe";
+        String[] Name = name.split(" ");
+        System.out.println(Arrays.toString(Name));
+
+    }
+}
