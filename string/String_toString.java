e85df5f1affa185dcc9b94962fd7b9249749bb1f
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:24:12 +0530

    String to string method

---
+++ string/String_toString.java
@@ -0,0 +1,8 @@
+package string;
+
+public class String_toString {
+    public static void main(String[] args) {
+        String name = "Radhe Radhe";
+        System.out.println(name.toString());
+    }
+}
