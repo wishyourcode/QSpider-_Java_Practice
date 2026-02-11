5a5714d3c7d4e3cee4dc87309b444d102a5fd165
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:28:37 +0530

    String toUpperCase method

---
+++ string/String_toUpperCase.java
@@ -0,0 +1,10 @@
+package string;
+
+public class String_toUpperCase {
+    public static void main(String[] args) {
+        String name = "Radhe Radhe";
+        String capName = name.toUpperCase();
+        System.out.println(name);
+        System.out.println(capName);
+    }
+}
