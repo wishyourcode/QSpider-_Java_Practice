64a38ae0c67bf5766d23f09da88fa89c2db8c4d7
wishyourcode <vishalm3101@gmail.com>
2026-02-11 23:31:12 +0530

    String charArray method

---
+++ string/String_charArray.java
@@ -0,0 +1,11 @@
+package string;
+
+import java.util.Arrays;
+
+public class String_charArray {
+    public static void main(String[] args) {
+        String name = "Radhe Radhe Radhe Radhe";
+        char[] Name = name.toCharArray();
+        System.out.println(Arrays.toString(Name));
+    }
+}
