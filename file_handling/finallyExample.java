233dd76fae5269c89253ade1d81bdf5e0a91a51f
wishyourcode <vishalm3101@gmail.com>
2026-02-12 23:57:42 +0530

    Finally block

---
+++ file_handling/finallyExample.java
@@ -0,0 +1,20 @@
+package file_handling;
+
+public class finallyExample {
+    public static void main(String[] args) {
+
+        int[] numbers = { 1, 2, 3 };
+        try {
+            // This will throw ArrayIndexOutOfBoundsException
+            System.out.println(numbers[5]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println("Exception caught: " + e);
+        } finally {
+            System.out.println("This block always executes.");
+        }
+        System.out.println("Program continues...");
+    }
+
+}
