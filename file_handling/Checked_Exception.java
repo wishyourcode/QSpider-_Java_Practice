afffc23ac1f4b063a3be994dbfa4adf42996109d
wishyourcode <vishalm3101@gmail.com>
2026-02-15 00:31:53 +0530

    checked exception handle

---
+++ file_handling/Checked_Exception.java
@@ -0,0 +1,25 @@
+package file_handling;
+
+import java.util.Scanner;
+
+public class Checked_Exception {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the 2 Number");
+        int l = input.nextInt();
+        int b = input.nextInt();
+        try {
+            System.out.println(calArea(l, b));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static int calArea(int l, int b) throws Exception {
+        if (l < 0 || b < 0) {
+            throw new Exception("Using negative number cant be calculate area");
+        } else {
+            return l * b;
+        }
+    }
+}
