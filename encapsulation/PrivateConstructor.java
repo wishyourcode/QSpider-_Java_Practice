55a4ab0e45da11d6b6d08276475a7b18943a82e9
wishyourcode <vishalm3101@gmail.com>
2026-02-16 23:43:51 +0530

    Private constructor

---
+++ encapsulation/PrivateConstructor.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class PrivateConstructor {
+    private int id;
+    private String Name;
+    private int rollno;
+
+    private PrivateConstructor(int id, String Name, int rollno) {
+        this.id = id;
+        this.Name = Name;
+        this.rollno = rollno;
+    }
+
+    public static PrivateConstructor createObj(int id, String Name, int rollno) {
+        return new PrivateConstructor(id, Name, rollno);
+    }
+
+    PrivateConstructor p1 = PrivateConstructor.createObj(1, "Vishal", 5);
+}
