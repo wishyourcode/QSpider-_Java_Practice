ac6db0d7ae93b98e39bc10e173def71194f87226
wishyourcode <vishalm3101@gmail.com>
2026-02-17 23:24:41 +0530

    ParseInt

---
+++ wrapper/Parse.java
@@ -0,0 +1,9 @@
+package wrapper;
+
+public class Parse {
+    public static void main(String[] args) {
+        String a = "12345";
+        int ch = Integer.parseInt(a);
+        System.out.println(ch);
+    }
+}
