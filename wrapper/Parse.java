0a7a72ee3190e107c12191a52d35c7bdf8d822f0
wishyourcode <vishalm3101@gmail.com>
2026-02-17 23:26:23 +0530

    string to primitive type conversion

--- wrapper/Parse.java
+++ wrapper/Parse.java
@@ -5,5 +5,8 @@ public class Parse {
         String a = "12345";
         int ch = Integer.parseInt(a);
         System.out.println(ch);
+        String b = "123456";
+        double d = Double.parseDouble(b);
+        System.out.println(d);
     }
 }
