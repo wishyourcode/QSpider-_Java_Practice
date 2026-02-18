9459a1704e3a632126a9a4f865aa7888ceb5967f
wishyourcode <vishalm3101@gmail.com>
2026-02-18 23:33:13 +0530

    Transpose matrix

---
+++ method/TransposeArray.java
@@ -0,0 +1,28 @@
+public class TransposeArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Transpose of the array:");
+        int[][] transpose = new int[arr[0].length][arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                transpose[j][i] = arr[i][j];
+            }
+        }
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
