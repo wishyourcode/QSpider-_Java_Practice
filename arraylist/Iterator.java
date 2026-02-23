ab6bae95a3353aeb37002d8b8e65faaafc0ae0cb
wishyourcode <vishalm3101@gmail.com>
2026-02-23 23:40:04 +0530

    Iterator clas

---
+++ arraylist/Iterator.java
@@ -0,0 +1,22 @@
+package arraylist;
+
+import java.util.*;
+
+public class Iterator {
+    public static void main(String[] args) {
+        ArrayList ls = new ArrayList();
+        ls.add(10);
+        ls.add(20);
+        ls.add(30);
+        ls.add(40);
+        ls.add(50);
+        ls.add(10);
+        System.out.println(ls);
+        java.util.Iterator i = ls.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+            System.out.println(i.hasNext());
+        }
+
+    }
+}
