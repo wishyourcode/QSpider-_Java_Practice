1da60f517e30ac3c46a78615812627333a886226
wishyourcode <vishalm3101@gmail.com>
2026-02-24 23:54:05 +0530

    Even number

---
+++ collection/assignment/EvenNeber.java
@@ -0,0 +1,45 @@
+package collection.assignment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ListIterator;
+
+import java.util.Iterator;
+
+public class EvenNeber {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+
+        ListIterator<Integer> i = al.listIterator();
+        Iterator<Integer> i2 = al.iterator();
+
+        // 2. Print even element sum
+
+        while (i.hasNext()) {
+            int temp = i.next();
+            if (temp % 2 == 0) {
+                System.out.println(temp);
+            }
+        }
+
+        for (int j = 0; j < al.size(); j++) {
+            if (al.get(j) % 2 == 0) {
+                System.out.println(al.get(j));
+            }
+        }
+
+        for (Integer num : al) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+
+        while (i2.hasNext()) {
+            int temp = i2.next();
+            if (temp % 2 == 0) {
+                System.out.println(temp);
+            }
+        }
+    }
+
+}
