a1c51671b0b27a74e27e0f81a366120cc4468245
wishyourcode <vishalm3101@gmail.com>
2026-02-24 23:55:33 +0530

    Missing NUmber

---
+++ collection/assignment/MissingNumber.java
@@ -0,0 +1,32 @@
+package collection.assignment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ListIterator;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 13, 14, 15));
+        Collections.sort(al);
+
+        ListIterator<Integer> i = al.listIterator();
+
+        while (i.hasNext()) {
+            int num1 = i.next();
+            int num2 = i.next();
+            if (num2 - num1 != 1) {
+                System.out.println(num1 + 1);
+            }
+            if (i.hasNext()) {
+                i.previous();
+            }
+        }
+
+        for (int j = 0; j < al.size() - 1; j++) {
+            if (al.get(j + 1) - al.get(j) != 1) {
+                System.out.println(al.get(j) + 1);
+            }
+        }
+    }
+}
