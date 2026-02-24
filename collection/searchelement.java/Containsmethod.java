76dbae0e2340799e0085b8c1cc2e717c54eff0e1
wishyourcode <vishalm3101@gmail.com>
2026-02-24 23:48:52 +0530

    Collection

---
+++ collection/searchelement.java/Containsmethod.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Containsmethod {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(2);
+        list.add(121);
+        list.add(1232);
+        list.add(1212);
+        System.out.println(list);
+        System.out.println("========================");
+        int keyElement = 121;
+        if (list.contains(keyElement)) {
+            System.out.println(keyElement + "Is present in list");
+        } else {
+            System.out.println(keyElement + "Is not present in list");
+        }
+    }
+}
