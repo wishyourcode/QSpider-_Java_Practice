fcbb9eeca46bdc34d4d698cc58d312a440d512d4
wishyourcode <vishalm3101@gmail.com>
2026-02-28 23:09:57 +0530

    Student vector class

---
+++ collection/vector/Student.java
@@ -0,0 +1,14 @@
+package vector;
+
+public class Student {
+    int stdNo;
+    String stdName;
+    String dName;
+
+    Student(int stdNo, String stdName, String dname) {
+        this.stdNo = stdNo;
+        this.stdName = stdName;
+        this.dName = dname;
+    }
+
+}
