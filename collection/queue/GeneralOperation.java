32eccc9d60efde5bf1ba9ddc1cd23f6791356688
wishyourcode <vishalm3101@gmail.com>
2026-03-02 23:54:18 +0530

    Queue insertion operationn

--- collection/queue/GeneralOperation.java
+++ collection/queue/GeneralOperation.java
@@ -5,6 +5,9 @@ import java.util.Queue;
 public class GeneralOperation {
     public static void main(String[] args) {
         Queue<Integer> q = new Queue<>();
+        q.add(null);
+        System.out.println(q.isEmpty());
+        System.out.println(q);
 
     }
 }
