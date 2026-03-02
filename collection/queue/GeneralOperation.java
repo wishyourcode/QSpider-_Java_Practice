2e911e79a00bc20c4f0b72b678cbc6468f5e2ec6
wishyourcode <vishalm3101@gmail.com>
2026-03-02 23:56:24 +0530

    enqueue operation

--- collection/queue/GeneralOperation.java
+++ collection/queue/GeneralOperation.java
@@ -1,13 +1,19 @@
 package queue;
 
 import java.util.Queue;
+import java.util.LinkedList;
 
 public class GeneralOperation {
     public static void main(String[] args) {
-        Queue<Integer> q = new Queue<>();
-        q.add(null);
-        System.out.println(q.isEmpty());
-        System.out.println(q);
+        Queue<Integer> q = new LinkedList<>();
 
+        System.out.println("Initially empty? " + q.isEmpty());
+
+        // Enqueue elements
+        q.add(10); // throws exception if it fails
+        q.offer(20); // returns false if it fails
+        q.offer(30);
+        System.out.println("After enqueues: " + q);
+        System.out.println("Size: " + q.size());
     }
 }
